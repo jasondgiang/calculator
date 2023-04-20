f3c4df9b04cb1cac388f837a08fa705e6473f7a9
Firewildstar <122087233+Firewildsar@users.noreply.github.com>
2023-04-20 11:47:31 -0700

    Function Operators

---
+++ calculator.js
@@ -0,0 +1,26 @@
+function addition(a,b){
+return a+b;
+}
+
+
+function subtraction(a,b){
+    return a-b;
+}
+
+function multiplication(a,b){
+    return a*b;
+}
+
+function division(a,b){
+    return a / b;
+}
+
+
+function operate(a,b){
+    division(a,b);
+}
+console.log(addition(1,2));
+console.log(subtraction(2,3));
+console.log(multiplication(5,5));
+console.log(division(1,3));
+console.log(operate(1,2));
